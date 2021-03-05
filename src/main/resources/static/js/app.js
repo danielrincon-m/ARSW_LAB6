66c2e8f2f4a7ed6891f26faa64516d84ec29c3dc
Daniel Rincón <daniel.rincon-m@mail.escuelaing.edu.co>
2021-03-04 23:53:33 -0500

    Front-End - Lógica

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,27 +1,55 @@
 var app = (function () {
 
     let authorName;
-    let blueprints = [];
+    let blueprints;
+    let totalPoints;
 
-    let getBlueprints = function(author) {
+    let getBlueprints = function (author) {
         authorName = author;
-        apimock.getBlueprintsByAuthor(author, function(err, res) {
-            for (let i = 0; i < res.length; i++) {
-                let blueprint = {
-                    name: res[i].author,
-                    size: res[i].points.length
-                };
-                blueprints.push(blueprint);
-            }
-//            console.log(blueprints);
+        totalPoints = 0;
+        blueprints = [];
+
+        apimock.getBlueprintsByAuthor(author, function (err, res) {
+            getBlueprintsInfo(res);
+            createTable();
         });
     };
 
-    let openBlueprint = function(name) {
+    let getBlueprintsInfo = function (res) {
+        for (let i = 0; i < res.length; i++) {
+            let blueprint = {
+                name: res[i].name,
+                nPoints: res[i].points.length
+            };
+            totalPoints += blueprint.nPoints;
+            blueprints.push(blueprint);
+        }
+    };
+
+    let createTable = function () {
+        let html = "<tr>" +
+            "<th>Blueprint name</th>" +
+            "<th>Number of points</th>" +
+            "<th>Open Blueprint</th>" +
+            "</tr>";
+        blueprints.forEach(bp => {
+            html += "<tr>";
+            html += "<td>" + bp.name + "</td>";
+            html += "<td>" + bp.nPoints + "</td>";
+            html += "<td></td>"; //Button
+            html += "</tr>";
+        });
+        $("#table-title").html(authorName + "'s blueprints");
+        $("#bp-table").html(html);
+        $("#total-points").html("Total user points: " + totalPoints);
+        // $("#current-bp-text").html("Current blueprint: " + )
+    };
+
+    let openBlueprint = function (name) {
         console.log("Open Blueprint: " + name);
     };
 
-    let changeAuthorName = function(newName) {
+    let changeAuthorName = function (newName) {
         authorName = newName;
     };
 
